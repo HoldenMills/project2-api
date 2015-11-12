b942155166c550323541dff3c7ad791b70260df2
Holden Mills <mholdenmills@gmail.com>
2015-11-12 14:27:48 -0500

    Created parks migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724141255) do
+ActiveRecord::Schema.define(version: 20151112192437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parks", force: :cascade do |t|
+    t.string  "name"
+    t.string  "designation"
+    t.string  "state"
+    t.integer "year_authorized"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
