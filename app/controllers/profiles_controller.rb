7f770fe58020da65a20f52260f618acb260f5662
Holden Mills <mholdenmills@gmail.com>
2015-11-13 09:45:05 -0500

    Fix syntax error - jrh

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,25 @@
 class ProfilesController < ApplicationController
   def create
-       @profile = Profile.new(profile_params)
-      if @profile.save
-        render json: @profile, status: :created, location: profile_url
-      else
-        render json: @profile.errors, status: :unprocessable_entity
-       end
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      render json: @profile, status: :created, location: profile_url
+    else
+      render json: @profile.errors, status: :unprocessable_entity
     end
   end
 
   def show
     @profile = Profile.find(params[:id])
-      render json: @profile
+    render json: @profile
   end
 
   def update
     @profile = Profile.find(params[:id])
-      if @profile.update(profile_params)
-        head :no_content
-      else
-        render json: @profile.errors, status: :unprocessable_entity
-      end
+    if @profile.update(profile_params)
+      head :no_content
+    else
+      render json: @profile.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -30,4 +29,3 @@ class ProfilesController < ApplicationController
     head :no_content
   end
 end
-
